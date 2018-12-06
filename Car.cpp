3cb13300f7fcfc58f35c697c03b93c470032b331
alessio <alessiocorsi74@gmail.com>
2018-12-06 10:23:55 +0100

    upgrade della classe Car

--- Car.cpp
+++ Car.cpp
@@ -1,25 +1,26 @@
+//
+// Created by alessio on 06/12/18.
+//
+
 #include "Car.h"
+
 #include <iostream>
 #include <math.h>
 
 Car::Car() {
-
 }
 
 bool Car::setMachinePlayer(int num_circuit, Sprite *S_MachinePlayer, Texture *T_MachinePlayer, float *x_CarPlayer, float *y_CarPlayer, double *degree_CarPlayer, double *degreeConst) {
-
     if (!T_MachinePlayer->loadFromFile("/home/alessio/Scrivania/Progetto Esame Programmazione/All_ultimo gas/race/macchina6.png")) {
         return true;
     }
     S_MachinePlayer->setTexture(*T_MachinePlayer);
     S_MachinePlayer->setOrigin(9.5, 0);
-
     switch(num_circuit) {
         case 1:
             *x_CarPlayer = 160;
             *y_CarPlayer = 312;
             *degreeConst = 180;
-
             break;
         case 2:
             *x_CarPlayer = 462;
@@ -34,14 +35,11 @@ bool Car::setMachinePlayer(int num_circuit, Sprite *S_MachinePlayer, Texture *T_
         default:
             break;
     }
-
     S_MachinePlayer->setPosition(Vector2f(*x_CarPlayer, *y_CarPlayer));
     S_MachinePlayer->setRotation(static_cast<float>(*degree_CarPlayer + *degreeConst));
     return false;
 }
-
 void Car::Car_Player(Sprite *S_MachinePlayer, float *CarPlayer_Acc, double *degree_CarPlayer, float *y_CarPlayer, float *x_CarPlayer, double *degreeConst, int num_circuit) {
-
     switch (control.SetControl(num_circuit, *y_CarPlayer, *x_CarPlayer, *degree_CarPlayer)) {
         case 0:
             Car_PlayerAcc(degreeConst, CarPlayer_Acc, y_CarPlayer, x_CarPlayer, degree_CarPlayer);
@@ -59,23 +57,18 @@ void Car::Car_Player(Sprite *S_MachinePlayer, float *CarPlayer_Acc, double *degr
             Car_PlayerRet(degreeConst, degree_CarPlayer, y_CarPlayer, x_CarPlayer);
             break;
         default: break;
-
     }
     S_MachinePlayer->setRotation(static_cast<float>(*degree_CarPlayer + *degreeConst));
     S_MachinePlayer->setPosition(Vector2f(*x_CarPlayer, *y_CarPlayer));
-
 }
 void Car::Car_PlayerRet(double *degreeConst, double *Degree_CarPlayer, float *y_CarPlayer, float *x_CarPlayer) {      //retromarcia
-
     if (Keyboard::isKeyPressed(Keyboard::Down))
     {
         *x_CarPlayer = static_cast<float>(*x_CarPlayer - 0.5 * cos(((*Degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
         *y_CarPlayer = static_cast<float>(*y_CarPlayer - 0.5 * sin(((*Degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
     }
 }
-
 void Car::Car_PlayerAcc(double *degreeConst, float *CarPlayer_Acc, float *y_CarPlayer, float *x_CarPlayer, double *degree_CarPlayer) { //accelerazione seguendo con freno motore
-
     if (Keyboard::isKeyPressed(Keyboard::Up))
     {
         start=1;
@@ -84,7 +77,6 @@ void Car::Car_PlayerAcc(double *degreeConst, float *CarPlayer_Acc, float *y_CarP
         }
         *x_CarPlayer = static_cast<float>(*x_CarPlayer + *CarPlayer_Acc * cos(((*degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
         *y_CarPlayer = static_cast<float>(*y_CarPlayer + *CarPlayer_Acc * sin(((*degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
-
     }
     else {
         if (0 < *CarPlayer_Acc && start == 1) {
@@ -94,32 +86,21 @@ void Car::Car_PlayerAcc(double *degreeConst, float *CarPlayer_Acc, float *y_CarP
         }
     }
 }
-
 void Car::Car_PlayerRi(double *degree_CarPlayer) {
-
     if (Keyboard::isKeyPressed(Keyboard::Right))     //incrementa l'angolo verso destra
-
         *degree_CarPlayer = *degree_CarPlayer + 0.7;
-
 }
-
 void Car::Car_PlayerLe(double *degree_CarPlayer) {
-
     if (Keyboard::isKeyPressed(Keyboard::Left))      //decrementa l'angolo verso sinistra
-
         *degree_CarPlayer = *degree_CarPlayer - 0.7;
-
 }
-
 void Car::Car_PlayerAcc_Out(double *degreeConst, float *CarPlayer_Acc, float *y_CarPlayer, float *x_CarPlayer, double *degree_CarPlayer) {
-
     if (Keyboard::isKeyPressed(Keyboard::Up))
     {
         start=1;
         *CarPlayer_Acc = 0.1;
         *x_CarPlayer = static_cast<float>(*x_CarPlayer + *CarPlayer_Acc * cos(((*degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
         *y_CarPlayer = static_cast<float>(*y_CarPlayer + *CarPlayer_Acc * sin(((*degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
-
     }
     else {
         if (0 < *CarPlayer_Acc && start == 1) {
@@ -128,187 +109,102 @@ void Car::Car_PlayerAcc_Out(double *degreeConst, float *CarPlayer_Acc, float *y_
             *y_CarPlayer = static_cast<float>(*y_CarPlayer + *CarPlayer_Acc * sin(((*degree_CarPlayer + *degreeConst + 90)* M_PI) / 180));
         }
     }
-
 }
-
 void Car::Car_PlayerRet_Out(double *degreeConst, double *Degree_CarPlayer, float *y_CarPlayer, float *x_CarPlayer) {
     if (Keyboard::isKeyPressed(Keyboard::Down))
     {
         *x_CarPlayer = static_cast<float>(*x_CarPlayer - 0.1 * cos(((*Degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
         *y_CarPlayer = static_cast<float>(*y_CarPlayer - 0.1 * sin(((*Degree_CarPlayer + *degreeConst + 90) * M_PI) / 180));
     }
-
 }
-
 void CarPlayer::setSoundBehavior() {
-
 }
-
 void CarPlayer::getSuoundBehavior() {
-
 }
-
 bool CarPlayer::getCarBehavior() {
-
     return false;
 }
-
 void CarPlayer::setUpgrade() {
-
 }
-
 void CarPlayer::setCarBehavior() {
-
 }
-
 void CarPlayer::getUpgrade() {
-
 }
-
 void CarPlayer::setBasetrajectory() {
-
 }
-
 bool CarPlayer::getBasetrajectory() {
     return false;
 }
-
 CarPlayer::CarPlayer() {
-
 }
-
 void CarBehavior::Accelerates() {
-
 }
-
 void CarBehavior::Restrains() {
-
 }
-
 void CarBehavior::Reverse_gear() {
-
 }
-
 void CarBehavior::GO_left() {
-
 }
-
 void CarBehavior::GO_right() {
-
 }
-
 SoundBehavior::SoundBehavior() {
-
 }
-
 void SoundBehavior::AlgorithmAcceleration() {
-
 }
-
 void SoundBehavior::AlgorithmBrake() {
-
 }
-
 void SoundBehavior::AlgorithmBase() {
-
 }
-
 void Basetrajectory::A_star() {
-
 }
-
 CarBehavior::CarBehavior() {
-
 }
-
 int Motore::Upgrade_Displacement() {
     return 0;
 }
-
 Motore::Motore() {
-
 }
-
 int Telaio::Upgrade_Weight() {
     return 0;
 }
-
 Telaio::Telaio() {
-
 }
-
 string Carrozzeria::Upgrade_Aerodynamics() {
     return std::__cxx11::string();
 }
-
 Carrozzeria::Carrozzeria() {
-
 }
-
 void CarsCPU::setCpuStrategy() {
-
 }
-
 void CarsCPU::getCpuStrategy() {
-
 }
-
 CarsCPU::CarsCPU() {
-
 }
-
 void SafetyCar::setCpuStrategy() {
-
 }
-
 void SafetyCar::getCpuStrategy() {
-
 }
-
 SafetyCar::SafetyCar() {
-
 }
-
 void CpuStrategy::A_star() {
-
 }
-
 int CpuStrategy::do_trajectory() {
     return 0;
 }
-
 bool CpuStrategy::get_SafetyCar() {
     return false;
 }
-
 CpuStrategy::CpuStrategy() {
-
 }
-
 void Trajectory::A_star1() {
-
 }
-
 Trajectory::Trajectory() {
-
 }
-
 void Trajectory_safetycar::A_star2() {
-
 }
-
 Trajectory_safetycar::Trajectory_safetycar() {
-
 }
-
 void GeneralTrajectory::A_star3() {
-
 }
-
 GeneralTrajectory::GeneralTrajectory() {
-
 }
-
-
-
-
--- Car.h
+++ Car.h
@@ -1,5 +1,9 @@
-#ifndef PROGETTO_CAR_H
-#define PROGETTO_CAR_H
+//
+// Created by alessio on 06/12/18.
+//
+
+#ifndef ALL_ULTIMO_GAS_CAR_H
+#define ALL_ULTIMO_GAS_CAR_H
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
@@ -9,7 +13,6 @@
 
 using namespace std;
 using namespace sf;
-
 
 class Car{
 
@@ -36,14 +39,9 @@ public:
     int Car_degree=0;
     float const_Acc=0.005, const_Brake=0.005;
 };
-
-
-
 class CarPlayer : public Car{
-
 protected:
     bool carUpgrade;
-
 public:
     void setSoundBehavior();
     void getSuoundBehavior();
@@ -53,10 +51,8 @@ public:
     void getUpgrade();
     void setBasetrajectory();
     bool getBasetrajectory();
-
     CarPlayer();
 };
-
 /* classe che definisce la macchina dell'utente
  */
 
@@ -64,29 +60,23 @@ class SoundBehavior : public CarPlayer{
 
 protected:
     string TypeofSound;
-
 public:
     void AlgorithmAcceleration();
     void AlgorithmBrake();
     void AlgorithmBase();
-
     SoundBehavior();
 };
-
 /* definizione della classe per il suono
  *
  */
-
 class Basetrajectory : public CarPlayer{
 
 public:
     void A_star();
 };
-
 /*
  * classe che definisce la traiettoria automatica del giocatore in caso di safety car
  */
-
 class CarBehavior : public CarPlayer{
 
 public:
@@ -95,124 +85,93 @@ public:
     void Reverse_gear();
     void GO_left();
     void GO_right();
-
     CarBehavior();
-
 };
-
 /* classe che definisce il comportamento della macchina in base all'utente
  *
  */
-
 class Motore : public CarPlayer{
-
 protected:
     int displacement;
-
 public:
     int Upgrade_Displacement();
-
     Motore();
 };
-
 /* classe motore per upgrade macchina giocatore
  *
  */
-
 class Telaio : public CarPlayer{
 
 protected:
     int weight;
-
 public:
     int Upgrade_Weight();
-
     Telaio();
 };
-
 /* classe telaio per upgrade macchina giocatore
  *
  */
-
 class Carrozzeria : public CarPlayer{
 
 protected:
     string Aerodynamics;
-
 public:
     string Upgrade_Aerodynamics();
-
     Carrozzeria();
 };
-
 class CarsCPU : public Car{
 
 public:
-
     CarsCPU();
-
     void setCpuStrategy();
     void getCpuStrategy();
-
 protected:
     string model;
 };
-
 /* classe che definisce le varie macchine del computer
  *
  */
-
 class SafetyCar : Car {
 
 protected:
     string colour2;
     void setCpuStrategy();
     void getCpuStrategy();
-
 public:
     SafetyCar();
-
 };
-
 /* classe che definisce la safety car
  *
  */
-
 class CpuStrategy{
 
 protected:
     bool OnSafetyCar;
     int Trajectory;
-
 public:
     void A_star();
     int do_trajectory();
     bool get_SafetyCar();
-
     CpuStrategy();
 };
-
 class Trajectory : public CpuStrategy{
 
 public:
     void A_star1();
-
     Trajectory();
 };
-
 class Trajectory_safetycar : public CpuStrategy{
 
 public:
     void A_star2();
-
     Trajectory_safetycar();
 };
-
 class GeneralTrajectory : public CpuStrategy{
 
 public:
     void A_star3();
-
     GeneralTrajectory();
 };
-#endif
+
+
+#endif //ALL_ULTIMO_GAS_CAR_H
