b6b5dc9c8723214b3e6b088185f11e57955e2bbe
alessio <alessiocorsi74@gmail.com>
2018-12-17 15:31:44 +0100

    upgrade della classe Circuit

--- Circuit.cpp
+++ Circuit.cpp
@@ -1,10 +1,6 @@
 #include "Circuit.h"
-
 Circuit::Circuit() {}
-
-
 bool Circuit::setTileMaps(int CircuitOn, Sprite *S_tilemaps, Texture *T_tilemaps, Sprite *S_Pause, Texture *T_Pause) {
-
     if(!T_Pause[0].loadFromFile("/home/alessio/Scrivania/Progetto Esame Programmazione/All_ultimo gas/race/termina.png")){
         return true;
     }
@@ -37,52 +33,14 @@ bool Circuit::setTileMaps(int CircuitOn, Sprite *S_tilemaps, Texture *T_tilemaps
     S_Pause[1].setTexture(T_Pause[1]);
     S_Pause[1].setPosition(874, -5);
     S_tilemaps->setTexture(*T_tilemaps);
-
     return false;
 }
-
 int Circuit::getTileMaps() {
-
     return 0;
-}
-
-bool Circuit::setWeather() {
-    return false;
-}
-
-bool Circuit::getWeather() {
-    return false;
 }
 
 int Circuit::getTile() {
     return 0;
 }
-
 Circuit::~Circuit() {
-
 }
-
-
-/*
-
-
-WeatherBehavior::WeatherBehavior() {}
-
-int WeatherBehavior::Do_weather() {
-    return 0;
-}
-
-Sun::Sun() {}
-
-void Sun::Do_weather() {
-    WeatherBehavior::Do_weather();
-}
-
-Rain::Rain() {}
-
-void Rain::Do_weather1() {
-
-}
-
-
-*/
--- Circuit.h
+++ Circuit.h
@@ -16,51 +16,16 @@ public:
 
     bool setTileMaps(int circuiton, Sprite *S_tilemaps, Texture *T_tilemaps, Sprite *S_Pause, Texture *T_Pause);
     int getTileMaps();
-    bool setWeather();
-    bool getWeather();
     int getTile();
-
     virtual ~Circuit();
 
 protected:
-
     int length;
     int number_of_curves;
     string difficult;
     string name;
     float record_lap;
     int number_race;
-
 };
-
-/*
-class WeatherBehavior{
-
-public:
-    WeatherBehavior();
-
-protected:
-    int Do_weather();
-};
-
-class Sun : public WeatherBehavior{
-
-public:
-    Sun();
-
-protected:
-    void Do_weather();
-};
-
-class Rain : public WeatherBehavior{
-public:
-    Rain();
-
-protected:
-    void Do_weather1();
-};
-*/
-
-
 
 #endif //PROGETTO_CIRCUIT_H
